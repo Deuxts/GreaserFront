133ed7bc0bae1c6f505cf62fed35d3d4cf069887
Deuxts <ariasdavidv@gmail.com>
2021-09-29 18:59:05 -0500

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Hash } from 'crypto';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./@public/pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./@public/pages/contact/contact.module').then(m => m.ContactModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: 'enabled',
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
