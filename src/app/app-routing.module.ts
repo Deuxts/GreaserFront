90bf2a462a26739c94deba07a98296e424bec958
David <ariasdavidv@gmail.com>
2021-09-29 20:06:11 -0500

    modificaciones menores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,24 +2,50 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Hash } from 'crypto';
 
-
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./@public/pages/home/home.module').then(m => m.HomeModule)
+    loadChildren: () =>
+      import('./@public/pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./@admin/pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'admin/users',
+    loadChildren: () =>
+      import('./@admin/pages/users/users.module').then(
+        (m) => m.UsersModule
+      ),
   },
   {
     path: 'contact',
-    loadChildren: () => import('./@public/pages/contact/contact.module').then(m => m.ContactModule)
-  }
+    loadChildren: () =>
+      import('./@public/pages/contact/contact.module').then(
+        (m) => m.ContactModule
+      ),
+  },{
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },{
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,
-    {
+  imports: [
+    RouterModule.forRoot(routes, {
       useHash: true,
       scrollPositionRestoration: 'enabled',
-    })],
-  exports: [RouterModule]
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
