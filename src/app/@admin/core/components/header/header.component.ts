130b3ce92af9c4dff7f9a7eacae2ff9d1c5fb6a7
Deux <danzet@outlook.com>
2021-09-30 15:05:33 -0500

    agregar estilos a sidebar

---
+++ src/app/@admin/core/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+
+  toggleValue = true;
+  @Output() toggleChange = new EventEmitter<boolean>();
+
+  toggled() {
+    if (this.toggleValue === undefined){
+      this.toggleValue = true;
+    }
+    this.toggleValue = !this.toggleValue;
+    console.log(this.toggleValue);
+    this.toggleChange.emit(this.toggleValue);
+  }
+
+}
